040b17c9f5b44a50325b7cfe9a8d30c7f5e9f88e
ajoy-code-dreamer <ajoy.code.dreamer@gmail.com>
2024-09-04 22:19:40 +0600

    banner desing complete

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// ===== jQuery operation =====
+$(function(){
+    // ==== banner slider ====
+    $('.banner_slider_wrapper').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        nextArrow: ".banner_next_arrow",
+        prevArrow: ".banner_prev_arrow",
+
+    });
+    // ==== banner slider ====
+    
+})
+// ===== jQuery operation =====
