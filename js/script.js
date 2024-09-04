83ef733ccb89cf1a3319cc5a68c0fc7f867c6227
ajoy-code-dreamer <ajoy.code.dreamer@gmail.com>
2024-09-05 00:26:48 +0600

    work on banner

--- js/script.js
+++ js/script.js
@@ -2,14 +2,25 @@
 $(function(){
     // ==== banner slider ====
     $('.banner_slider_wrapper').slick({
-        infinite: true,
         slidesToShow: 1,
         slidesToScroll: 1,
+        speed: 1000,
+        swipeToSlide:true,
         nextArrow: ".banner_next_arrow",
         prevArrow: ".banner_prev_arrow",
-
     });
     // ==== banner slider ====
     
+    
 })
 // ===== jQuery operation =====
+
+// ==== type js ====
+var typed3 = new Typed('.typed', {
+    strings: ['My strings are: <i>strings</i> with', 'My strings are: <strong>HTML</strong>', 'My strings are: Chars &times; &copy;'],
+    typeSpeed: 0,
+    backSpeed: 0,
+    smartBackspace: true, // this is a default
+    loop: true
+});
+// ==== type js ====
