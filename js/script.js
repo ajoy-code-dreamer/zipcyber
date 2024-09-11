54127c3afa2f97a13d7fb60d56154a8ae9b1a25e
ajoy-code-dreamer <ajoy.code.dreamer@gmail.com>
2024-09-11 18:29:54 +0600

    added banner design and some feature

--- js/script.js
+++ js/script.js
@@ -1,26 +1,31 @@
 // ===== jQuery operation =====
-$(function(){
-    // ==== banner slider ====
-    $('.banner_slider_wrapper').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        speed: 1000,
-        swipeToSlide:true,
-        nextArrow: ".banner_next_arrow",
-        prevArrow: ".banner_prev_arrow",
+$(function () {
+  // ==== banner slider ====
+$(".banner_slider_wrapper").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    speed: 1500,
+    swipeToSlide: true,
+    nextArrow: ".banner_next_arrow",
+    prevArrow: ".banner_prev_arrow",
+    infinity:true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    adaptiveHeight: true,
     });
-    // ==== banner slider ====
-    
-    
-})
+  // ==== banner slider ====
+});
 // ===== jQuery operation =====
 
 // ==== type js ====
-var typed3 = new Typed('.typed', {
-    strings: ['My strings are: <i>strings</i> with', 'My strings are: <strong>HTML</strong>', 'My strings are: Chars &times; &copy;'],
-    typeSpeed: 0,
-    backSpeed: 0,
-    smartBackspace: true, // this is a default
-    loop: true
+var typed = new Typed(".typed", {
+    strings: [
+    "Welcome to Zip Cyber",
+    "The best internet service in this Area",
+    "Connect Faster - Live Better.",
+],
+    typeSpeed: 60,
+    backSpeed: 60,
+    loop: true,
 });
 // ==== type js ====
